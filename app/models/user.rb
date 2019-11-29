70f97102cfa3048a3689c51dc1e5f97622b146de
Andy's  Blog <andycurran82@gmail.com>
2019-11-29 01:49:43 -0600

    fixed user.rb article dependency

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     has_many :articles, dependent: :destroy
-    has_many :articles
     before_save { self.email = email.downcase }
     validates :username, 
                 presence: true, 
