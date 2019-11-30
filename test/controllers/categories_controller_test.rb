262439d1440d2e42cd5ada99021bc2744ab96979
Andy's  Blog <andycurran82@gmail.com>
2019-11-30 00:24:57 -0600

    fix category admin bugs

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,7 +4,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
     def setup
         @category = Category.create(name: "sports")
-        @user = User.create(username: "john", email: "john@eample.com", password: "password", admin: true)
+        @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
     end
 
     test "should get categories index" do 
@@ -20,5 +20,12 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     test "should get show" do 
         get category_path(@category)
         assert_response :success
+    end 
+
+    test "should redirect create when admin not logged in " do 
+        assert_no_difference 'Category.count' do 
+            post categories_path, params: {category: { name: "sports" } }
+        end
+        assert_redirected_to categories_path
     end 
 end
